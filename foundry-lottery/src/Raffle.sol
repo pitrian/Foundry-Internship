0103c29c304b5b6872f07965230143e994306a59
Minh Chung <chungngonbkqna@gmail.com>
2026-06-04 23:56:28 +0700

    error

--- foundry-lottery/src/Raffle.sol
+++ foundry-lottery/src/Raffle.sol
@@ -2,25 +2,39 @@
 pragma solidity ^0.8.18;
 
 /**
- *  @title A sample Raffle Contract
- *  @author Minh Chung
- *  @notice This contract is for creating a sample raffle
- *  @dev It implements Chainlink VRFv2.5 and Chainlink Automation
+ * @title A sample Raffle Contract
+ * @author Patrick Collins
+ * @notice This contract is for creating a sample raffle
+ * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
  */
-
 contract Raffle {
-    // Biến trạng thái (State variables)
+    /* Errors */
+    // Việc sử dụng tiền tố Raffle__ giúp dễ dàng xác định lỗi đến từ hợp đồng nào [5].
+    error Raffle__NotEnoughEthSent();
+
+    /* State variables */
+    // Biến immutable được lưu trong bytecode, giúp truy xuất rẻ hơn biến storage [6, 7].
     uint256 private immutable i_entranceFee;
 
-    // Hàm khởi tạo (Constructor)
+    /* Functions */
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    // Hàm tham gia xổ số
-    function enterRaffle() public payable {}
+    /**
+     * @notice Hàm tham gia xổ số
+     * @dev Thay đổi từ public sang external để tiết kiệm gas vì không gọi nội bộ [1].
+     */
+    function enterRaffle() external payable {
+        // Sử dụng mô hình if/revert với Custom Error thay vì require(string) để tối ưu gas [2, 5].
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+    }
 
-    // Hàm chọn người chiến thắng (Sẽ triển khai logic ở các bài sau)
+    /**
+     * @notice Hàm chọn người chiến thắng (Sẽ được triển khai logic ở các bài sau)
+     */
     function pickWinner() public {}
 
     /** Getter Function */
